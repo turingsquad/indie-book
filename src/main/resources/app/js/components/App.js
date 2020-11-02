61184bcdda362612ba1b566df486281b26037e94
Arthasasyan <minearthas@gmail.com>
2020-11-02 11:03:44 +0300

    Added material UI and simple routing

--- src/main/resources/app/js/components/App.js
+++ src/main/resources/app/js/components/App.js
@@ -1,16 +1,27 @@
 import React, {Component} from "react";
 import editor from "./TextEditor.js"
+import Header from "./header/Header";
+import Section from "./header/sections"
 
 import '../../styles/App.css';
+import Books from "./Books";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Container from "@material-ui/core/Container";
+import RoutingHeader from "./header/RoutingHeader";
+
+const sections = [
+    new Section("Книги", "/books")
+]
 
 class App extends Component {
     render() {
         return (
-            <div>
-                <button name="Edit" onClick={editor.create()}>
-                    <h1>Text Editor</h1>
-                </button>
-            </div>
+            <React.Fragment>
+                <CssBaseline />
+                <Container maxWidth="lg">
+                    <RoutingHeader title="Indie Book" sections={sections}/>
+                </Container>
+            </React.Fragment>
         );
     }
 }
