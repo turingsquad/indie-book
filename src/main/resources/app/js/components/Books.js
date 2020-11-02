3b68255a9555ca49ea2a06437635e179a6a1d5fd
Arthasasyan <minearthas@gmail.com>
2020-11-02 11:03:44 +0300

    Added material UI and simple routing

---
+++ src/main/resources/app/js/components/Books.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+
+export default class Books extends React.Component{
+    render() {
+        return(
+            <div>
+                <h1>Кнежке</h1>
+            </div>
+        )
+    }
+}
