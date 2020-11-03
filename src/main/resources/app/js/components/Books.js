2cb02806af17a7bcd34f79b25d114399521c9739
Arthasasyan <minearthas@gmail.com>
2020-11-03 11:44:40 +0300

    Frontend template

--- src/main/resources/app/js/components/Books.js
+++ src/main/resources/app/js/components/Books.js
@@ -1,12 +1,30 @@
 import * as React from "react";
+import Book from "./entities/Book";
+import "regenerator-runtime/runtime.js";
 
+function getBooksByUser(userId) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "/api/v1/books/" + userId, false);  // synchronous request
+    xhr.send(null);
+    let json = JSON.parse(xhr.responseText)
+    console.log(json)
+    return json
+}
 
-export default class Books extends React.Component{
+export default class Books extends React.Component {
     render() {
-        return(
-            <div>
-                <h1>Кнежке</h1>
-            </div>
+        let bookList = getBooksByUser(1)
+        return (
+            <React.Fragment>
+                {bookList.map((book) => (
+                        <Book id={book.id}
+                              userId={book.userId}
+                              creationDate={book.creationDate}
+                              chapterCount={book.chapterCount}/>
+                    )
+                )
+                }
+            </React.Fragment>
         )
     }
 }
